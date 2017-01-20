e8a5e6ebd3c288e16f97a5b09c08ecf363f42869
Ilya1991 <dp020891miv@gmail.com>
2017-01-20 21:25:58 +0200

    Add files via upload

---
+++ test/test_drawer.py
@@ -0,0 +1,16 @@
+from gendraw import GeneticDrawer
+from PIL import Image
+import os
+
+
+__author__ = 'Ilya'
+
+
+if __name__ == '__main__':
+    pic_path = os.path.join(os.path.abspath('../'), 'pic')
+    pic_name = 'pb.png'
+    pic_filename = os.path.join(pic_path, pic_name)
+    pic = Image.open(pic_filename)
+    pic = pic.convert('RGB')
+    drawer = GeneticDrawer(pic, n_circles=256, n_generations=1000000, path='C:/nba/')
+    drawer.draw()
